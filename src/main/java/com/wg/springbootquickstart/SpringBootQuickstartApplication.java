4a115a840403a18a574a20ad23171ed27bc1848c
xuanyuan2016 <xiongbinhui100@163.com>
2018-06-23 14:43:50 +0800

    'init'

---
+++ src/main/java/com/wg/springbootquickstart/SpringBootQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.wg.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickstartApplication.class, args);
+	}
+}
